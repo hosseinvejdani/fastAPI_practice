936e25aa9354d455c1ced8c3aef0c6613cbdfb00
hosseinvejdani <vejdani.shojae@gmail.com>
2022-08-03 08:24:22 +0430

    how to use optinal query parameters in fastapi

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = FastAPI()
 
 
 
-# how to use path and query parameters at the same time in fastapi ------------------------------
-@app.get('/blog/{id}/comments/{comment_id}')
-def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
-    return {"message": f"blog id {id} comment id {comment_id} {valid=} {username=}"}
+
+
+# how to use optinal query parameters in fastapi ---------------------------------------------------
+@app.get('/blog/all')
+def get_blogs(page: Optional[int] = None, page_size: str = None):
+    return {"message": f"{page=} -- {page_size=}"}
 
 
