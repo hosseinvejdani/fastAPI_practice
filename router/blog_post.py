b2c2263bf6b3fe05e506e19738cf3c0994f37197
hosseinvejdani <vejdani.shojae@gmail.com>
2022-08-03 09:25:56 +0430

    this file shows you how to use post method and base model

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
+
+'''
+this file shows you how to use post method and base model
+'''
 
 router = APIRouter(prefix='/blog', tags=['blog'])
 
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+
+
 @router.post('/new')
-def create_blog():
-    return {"message": 'OK'}
+def create_blog(blog: BlogModel):
+    return {"message": 'OK', "data": blog}
